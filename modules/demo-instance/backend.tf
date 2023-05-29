0cc6ca4aacea470e02be3fc481abb7c2eed16458
Nate Thompson <haplo@haplolabs.io>
2023-05-29 14:55:55 -0600

    Testing new

--- modules/demo-instance/backend.tf
+++ modules/demo-instance/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "haplolabs-home-tf-state"
-    key = "demo-instance/terraform.tfstate"
+    key    = "new-demo-instance/terraform.tfstate"
 
     # endpoint = set via -backend-config="endpoint=${s3_endpoint}"
     # access_key = set via -backend-config="access_key=${access_key}"
@@ -9,8 +9,8 @@ terraform {
     # region = set via -backend-config="state_region=${state_region}"
 
     skip_credentials_validation = true
-    skip_metadata_api_check = true
-    skip_region_validation = true
-    force_path_style = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
   }
 }
